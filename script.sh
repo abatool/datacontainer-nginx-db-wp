8fda6b06a7a77f15a434b5cc124f3d3ffe5eacbf
abatool <32073910+abatool@users.noreply.github.com>
2018-02-10 18:37:13 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ echo "--------------------------------------------------------------------------
 docker network create wpnet
 
 echo "------------------------------------------------------------------------------------------------------"
-echo "----------Creating a container named datacontainer with the image abatool1/datacontainer-nginx-db-wp which maps volumes /usr/share/nginx/html for nginx and /var/lib/mysql for mariadb.---------------"
+echo "----------Creating a container named datacontainer with the image abatool1/datacontainer-nginx-db-wp which maps volumes /var/www/html for nginx and /var/lib/mysql for mariadb.---------------"
 echo "------------------------------------------------------------------------------------------------------"
 
 docker create --name datacontainer --network wpnet abatool1/datacontainer-nginx-db-wp
